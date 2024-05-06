9e93eb13a65d07ed33d92a08297695b0a6903243
= <=>
2024-05-06 22:57:38 +0000

    added home-manager

--- flake.nix
+++ flake.nix
@@ -7,17 +7,31 @@
       url = "github:tpwrules/nixos-apple-silicon";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-
-    nixosConfigurations.coppermind-nix-asahi = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      specialArgs = inputs;
-      modules = [
-        ./configuration.nix
-      ];
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
+
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "aarch64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.coppermind-nix-asahi = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        specialArgs = inputs;
+        modules = [ ./configuration.nix ];
+      };
+      homeConfigurations."yajj" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
