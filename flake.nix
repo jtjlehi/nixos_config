e8bf5924cfe2c3a63d3e7a0333df182fd86a87ae
= <=>
2024-05-28 08:56:22 -0600

    fixed formatting and removed dead code

--- flake.nix
+++ flake.nix
@@ -18,51 +18,24 @@
     home-manager,
     ...
   } @ inputs: let
-    host = system: host_config: nixpkgs.lib.nixosSystem {
-      inherit system;
-      pkgs = import nixpkgs {inherit system;};
-      specialArgs = inputs;
-      modules = [
-        ./configuration.nix
-	host_config
-	home-manager.nixosModules.home-manager {
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users."yajj" = import ./home;
-	}
-      ];
-    };
-    # system = "aarch64-linux";
-  in {
-    # nixos configurations
-    nixosConfigurations.pewtermind = host "x86_64-linux" ./pewtermind.nix;
-    # nixosConfigurations.pewtermind = nixpkgs.lib.nixosSystem {
-    #   system = "aarch64-linux";
-    #   pkgs = import nixpkgs {inherit system;};
-    #   specialArgs = inputs;
-    #   modules = [./configuration.nix];
-    # };
-    nixosConfigurations.coppermind = host "aarch64-linux" ./asahi-config.nix;
- #    nixpkgs.lib.nixosSystem {
- #      system = "aarch64-linux";
- #      pkgs = import nixpkgs {inherit system;};
- #      specialArgs = inputs;
- #      modules = [./configuration.nix];
- #    };
-    # home configurations
-    homeConfigurations."yajj" = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs {
-        system = "aarch64-linux";
-        # inherit system;
-        overlays = [
-          inputs.apple-silicon.overlays.apple-silicon-overlay
-          (final: prev: {mesa = final.mesa-asahi-edge;})
+    host = system: host_config:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        pkgs = import nixpkgs {inherit system;};
+        specialArgs = inputs;
+        modules = [
+          ./configuration.nix
+          host_config
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."yajj" = import ./home;
+          }
         ];
       };
-      extraSpecialArgs = {inherit inputs;};
-      modules = [
-        ./home
-      ];
-    };
+  in {
+    nixosConfigurations.pewtermind = host "x86_64-linux" ./pewtermind.nix;
+    nixosConfigurations.coppermind = host "aarch64-linux" ./asahi-config.nix;
   };
 }
