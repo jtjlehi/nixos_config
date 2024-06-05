1fe998d8c4d7e3ec7a5ca821ce2c13cf2d2a753b
= <=>
2024-06-04 20:10:28 -0600

    added cmp_nvim_lsp capabilities to listing

--- home/nvim/lsp.lua
+++ home/nvim/lsp.lua
@@ -63,6 +63,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
 ---------------------------------
 -- Language Specific LSP Setup --
 ---------------------------------
