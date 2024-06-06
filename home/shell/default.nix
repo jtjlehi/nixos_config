36395f1ccb455745eb419cfad37af78f1db148ac
= <=>
2024-06-05 23:09:29 -0600

    added writeFlagScript function for shells

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,11 +1,14 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }: let
+  inherit (pkgs) writeShellScriptBin;
+  inherit (lib.strings) concatMapStringsSep stringLength;
   gitBin = "${pkgs.git}/bin/git";
   writeFzfScript = name: choices: cmd:
-    pkgs.writeShellScriptBin name ''
+    writeShellScriptBin name ''
       CHOICE=$(${choices} | ${pkgs.fzf}/bin/fzf)
       echo ${name} $CHOICE
       ${cmd} $CHOICE
@@ -26,6 +29,69 @@
     sedPat = ''s/\s*origin\/((\w[^\s])*).*/\1/'';
     gitCmd = "pull origin";
   };
+
+  g-push = writeFzfGitScript "g-push" {
+    branchOpt = "-l";
+    sedPat = "s/^..//";
+    gitCmd = "push origin";
+  };
+  pull-build = pkgs.writeShellScriptBin "pull-build" ''
+    cd ${config.home.homeDirectory}/.dotfiles/nixos
+    OUT=$(${g-pull}/bin/g-pull)
+    echo $OUT
+    if [[ $OUT == *"Already up to date."* ]];
+    then
+      echo "Nothing to do"
+    else
+      sudo nixos-rebuild switch --flake .
+    fi
+  '';
+  writeFlagScript = name: {
+    subCommands ? [],
+    flags ? [],
+    body ? "",
+  }: let
+    parsedFlags =
+      concatMapStringsSep "\n" (opts: ''
+        ${concatMapStringsSep "|" (opt:
+          (
+            if stringLength opt == 1
+            then "-"
+            else "--"
+          )
+          + opt)
+        opts})
+        ${builtins.head opts}=$2
+        shift;;
+      '')
+      flags;
+    parseSub = concatMapStringsSep "\n" ({subName, ...}: "${subName}) ${subName}=true;;") subCommands;
+    handleSub =
+      concatMapStringsSep "\n" (
+        {
+          subName,
+          subBody,
+        }: ''
+          if [[ ''$${subName} ]]; then
+            ${subBody}
+          fi
+        ''
+      )
+      subCommands;
+    script = ''
+      while [ $# -gt 0 ]
+      do
+        case $1 in
+        ${parsedFlags}
+        ${parseSub}
+        esac
+        shift
+      done
+      ${handleSub}
+      ${body}
+    '';
+  in
+    writeShellScriptBin name script;
 in {
   programs.bash.enable = true;
   home.sessionVariables = {
@@ -39,23 +105,9 @@ in {
     enableBashIntegration = true;
     nix-direnv.enable = true;
   };
-  home.packages = let
-  in [
-    (writeFzfGitScript "g-push" {
-      branchOpt = "-l";
-      sedPat = "s/^..//";
-      gitCmd = "push origin";
-    })
-    (pkgs.writeShellScriptBin "pull-build" ''
-      cd ${config.home.homeDirectory}/.dotfiles/nixos
-      OUT=$(${g-pull}/bin/g-pull)
-      echo $OUT
-      if [[ $OUT == *"Already up to date."* ]];
-      then
-        echo "Nothing to do"
-      else
-        sudo nixos-rebuild switch --flake .
-      fi
-    '')
+  home.packages = [
+    pull-build
+    g-pull
+    g-push
   ];
 }
