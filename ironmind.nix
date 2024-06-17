cf9ed34b73e7c171d1ee1fafcd137f153f67c31a
jtjlehi <jtjlehi@gmail.com>
2024-06-17 15:34:08 -0600

    added slack to ironmind

--- ironmind.nix
+++ ironmind.nix
@@ -1,7 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   imports = [hardware/iron.nix];
   anduril-security = {
     kolide-launcher.enable = true;
     cloudflare-warp.enable = true;
   };
+  environment.systemPackages = with pkgs; [
+    slack
+  ];
 }
