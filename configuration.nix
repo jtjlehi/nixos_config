9ee713b41b9ccf6ae9bdc653c4b83bb3c5229c5a
jtjlehi <jtjlehi@gmail.com>
2024-07-25 18:04:15 -0600

    Updated nix settings

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,8 @@
   ...
 }: {
   imports = [./styling];
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = ["nix-command" "flakes" "fetch-closure"];
+  nix.settings.trusted-users = [ "root" "yajj" ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
