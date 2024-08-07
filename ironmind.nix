54def1630198d728595cfd772c86ab81a8c4a261
jtjlehi <jtjlehi@gmail.com>
2024-08-06 18:13:18 -0600

    Reconfigured ironmind builders and stuff

--- ironmind.nix
+++ ironmind.nix
@@ -9,30 +9,29 @@
   ];
   boot.initrd.luks.devices."luks-57691d44-253b-4274-a395-e1de76de708d".device = "/dev/disk/by-uuid/57691d44-253b-4274-a395-e1de76de708d";
   # the cachix setup is not completely declarative
-  nix.settings = let
-    subs = [ "https://cache.nixos.org/" ];
-  in {
-    substituters = subs;
-    trusted-substituters = subs;
-    builders-use-substitutes = true;
-    trusted-public-keys = [
-      "anduril-core-nix-cache.cachix.anduril.dev-1:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
-      "anduril-pulsar-nix-cache.cachix.anduril.dev-1:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
-      "polyrepo.cachix.anduril.dev-1:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
+  nix = {
+    settings = rec {
+      substituters = [ "https://cache.nixos.org/" ];
+      trusted-substituters = substituters;
+      builders-use-substitutes = true;
+      trusted-public-keys = [
+        "anduril-core-nix-cache.cachix.anduril.dev-1:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
+        "anduril-pulsar-nix-cache.cachix.anduril.dev-1:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
+        "polyrepo.cachix.anduril.dev-1:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
+      ];
+    };
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "anduril@192.168.70.9";
+        system = "aarch64-linux";
+        sshKey = "/root/.ssh/nixos_build_server_key";
+        maxJobs = 64;
+        speedFactor = 64;
+        supportedFeatures = ["big-parallel" "kvm" "benchmark"];
+      }
     ];
-    builders = "@/etc/nix/machines";
   };
-  nix.distributedBuilds = true;
-  nix.buildMachines = [
-    {
-      hostName = "ssh://anduril@192.168.70.9";
-      system = "aarch64-linux";
-      sshKey = "/root/.ssh/nixos_build_server_key";
-      maxJobs = 64;
-      speedFactor = 64;
-      supportedFeatures = ["big-parallel" "kvm" "benchmark"];
-    }
-  ];
   programs.ssh.extraConfig = ''
     Host ghe.anduril.dev
       HostName ghe.anduril.dev
