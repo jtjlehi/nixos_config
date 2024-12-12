57941768c6bfb7eab813dff0e51bca76a456c22e
neoj <jjacobson.ctr@anduril.com>
2024-12-12 11:28:34 -0700

    enabled hardware graphics

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,8 @@
     generateCaches = true;
   };
 
+  hardware.graphics.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
