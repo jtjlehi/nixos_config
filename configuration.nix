1cac537f29ae1ce2c7301f97131d5553639a5dcb
jtjlehi <jtjlehi@gmail.com>
2025-01-30 12:14:10 -0700

    added podman

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,9 @@
     nushell
     zsh
     pulseaudio
+    # podman/docker
+    podman-tui
+    podman-compose
   ];
   documentation.man = {
     enable = true;
@@ -76,6 +79,21 @@
   };
 
   hardware.graphics.enable = true;
+
+  # podman/docker
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
