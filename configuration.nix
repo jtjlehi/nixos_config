4685f45d55917ab9d15f9b943b7fbbd0a4c9315b
neoj <jjacobson.ctr@anduril.com>
2025-10-21 11:12:37 -0600

    Updated the perf dependency

--- configuration.nix
+++ configuration.nix
@@ -8,11 +8,6 @@
   nix.settings.experimental-features = ["nix-command" "flakes" "fetch-closure"];
   nix.settings.trusted-users = [ "root" "yajj" ];
   nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    randomizedDelaySec = "14m";
-    options = "--delete-older-than 100d";
-  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -80,7 +75,7 @@
     python3
     valgrind
     bat
-    linuxPackages.perf
+    perf
     # plotting and diagrams and visuals and stuff
     gnuplot
     eog
