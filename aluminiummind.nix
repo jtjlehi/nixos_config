41538819c9e6006e4e8c9ecac2511855c52b5c15
neoj <jjacobson.ctr@anduril.com>
2026-02-16 13:43:05 -0700

    Added correct email for aluminiummind git config

--- aluminiummind.nix
+++ aluminiummind.nix
@@ -11,4 +11,10 @@
   users.users.${config.username} = {
     home = "/home/jjacobson";
   };
+  home-manager.users.${config.username} = { ... }: {
+    programs.git.settings.user = lib.mkForce {
+      name = "neoj";
+      email = "jjacobson.ctr@anduril.com";
+    };
+  };
 }
